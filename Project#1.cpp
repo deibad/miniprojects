e00c8e8edd5d4431e69f0b696ee30ecd7ab1d929
deibad <hammamibad7@gmail.com>
2026-05-27 23:21:11 +0700

    first commit

--- Project#1.cpp
+++ Project#1.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <cstdlib>
 #include <windows.h>
+using namespace std;
+
+struct user{
+    string username;
+    string email;
+    string password;
+};
 
 
 
 
+void entrance();
 
+void entrance(){
+    cout << "WELCOME TO tOkOBook\n";
+    system("pause");
+    system("cls");
+    cout << "Please Read This Carefully\n";
+    system("pause");
+    system("cls");
+}
+
+int main(){
+    entrance();
+}
